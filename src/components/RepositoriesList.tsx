d107ff04f4df12408704eb5f2d0e329f3be263ae
YoshijiFujiwara <yfbiology2@gmail.com>
2021-03-29 23:55:48 +0900

    use typed selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -16,6 +20,9 @@ const RepositoriesList: React.FC = () => {
       <form onSubmit={onSubmit}>
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
+        {error && <h3>{error}</h3>}
+        {loading && <h3>Loading...</h3>}
+        {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
       </form>
     </div>
   );
